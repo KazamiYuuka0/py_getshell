548e7ebb8b2d0290031f29e095de30a889134677
BoTao Fang <123790230+KazamiYuuka0@users.noreply.github.com>
2024-08-22 13:32:28 +0800

    Delete ver0.7（文件加密） directory

--- ver0.7（文件加密）/dist/test.py
+++
@@ -1,8 +0,0 @@
-import threading
-import subprocess
-def thread_(path):
-    subprocess.call(path)
-    print('over')
-    return 0
-thread1 = threading.Thread(target=thread_,args=('gamedata.dll',))
-thread1.start()
